2d17a4233b9e887b6146eb0cb12ce9693a59acd1
bacebu4 <vasua14735@icloud.com>
2021-02-11 07:05:59 +0300

    init with layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const SidebarContent = styled.aside`
+  width: 33%;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  overflow: auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <SidebarContent>
+        <h1>hey</h1>
+      </SidebarContent>
+      <MainContent>
+        <h2>cool here</h2>
+        <h2>cool here</h2>
+        <h2>cool here</h2>
+        <h2>cool here</h2>
+        <h2>cool here</h2>
+      </MainContent>
+    </Wrapper>
   );
 }
 
