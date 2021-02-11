329872b1098950b8d8cc1345d15d843fa14bd8f5
bacebu4 <vasua14735@icloud.com>
2021-02-11 20:55:36 +0300

    feat: flexBox component created

---
+++ src/components/FlexBox.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+interface FlexBoxProps {
+  jc?:
+    | "space-around"
+    | "space-between"
+    | "space-evenly"
+    | "stretch"
+    | "center"
+    | "end"
+    | "flex-end"
+    | "flex-start";
+  ai?:
+    | "center"
+    | "end"
+    | "flex-end"
+    | "flex-start"
+    | "self-end"
+    | "self-start"
+    | "start"
+    | "baseline"
+    | "normal"
+    | "stretch";
+  mt?: string;
+  ml?: string;
+}
+
+export const FlexBox = styled.div<FlexBoxProps>`
+  display: flex;
+  justify-content: ${(props) => props.jc};
+  align-items: ${(props) => props.ai};
+  margin-left: ${(props) => props.ml};
+  margin-top: ${(props) => props.mt};
+`;
