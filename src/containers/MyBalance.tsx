f989af690f0bfb8abab15497903f6d88a055190d
bacebu4 <vasua14735@icloud.com>
2021-02-12 07:33:13 +0300

    refactor: deleted unused

--- src/containers/MyBalance.tsx
+++ src/containers/MyBalance.tsx
@@ -15,34 +15,6 @@ const BalanceAmount = styled.h1<{ mt?: string }>`
   margin-top: ${(props) => props.mt};
 `;
 
-const data = [
-  {
-    id: "ruby",
-    label: "ruby",
-    value: 318,
-  },
-  {
-    id: "hack",
-    label: "hack",
-    value: 237,
-  },
-  {
-    id: "sass",
-    label: "sass",
-    value: 558,
-  },
-  {
-    id: "python",
-    label: "python",
-    value: 209,
-  },
-  {
-    id: "haskell",
-    label: "haskell",
-    value: 567,
-  },
-];
-
 const GrayText = styled.span`
   color: ${GRAY};
   font-weight: 500;
