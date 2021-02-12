5944c46b3ecbe342bdfddc8153b0bc5384711a28
bacebu4 <vasua14735@icloud.com>
2021-02-12 06:52:50 +0300

    feat: helper function for colors

--- src/colors.ts
+++ src/colors.ts
@@ -8,3 +8,52 @@ export const LIGHT_GRAY = "#F7F6FF";
 export const GREEN = "#56C13B";
 export const RED = "#D68927";
 export const DARK = "#383838";
+
+export type Color =
+  | "purple"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "dark blue"
+  | "rose"
+  | "gray"
+  | "light gray"
+  | "red"
+  | "dark";
+
+export function colorSwitcher(color: Color) {
+  switch (color) {
+    case "blue":
+      return BLUE;
+
+    case "purple":
+      return PURPLE;
+
+    case "yellow":
+      return YELLOW;
+
+    case "green":
+      return GREEN;
+
+    case "dark blue":
+      return DARK_BLUE;
+
+    case "rose":
+      return ROSE;
+
+    case "gray":
+      return GRAY;
+
+    case "light gray":
+      return LIGHT_GRAY;
+
+    case "red":
+      return RED;
+
+    case "dark":
+      return DARK;
+
+    default:
+      break;
+  }
+}
