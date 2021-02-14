90f96ff8f0010eba5d7d5f79a2dc01d3ac801a11
bacebu4 <vasua14735@icloud.com>
2021-02-14 12:15:39 +0300

    layout: added modal window

--- web/src/containers/MyBalance.tsx
+++ web/src/containers/MyBalance.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { Title } from "../components/Title";
 import { GRAY, PURPLE, DARK } from "../utils/colors";
@@ -8,6 +8,8 @@ import { FlexBox } from "../components/FlexBox";
 import { mockData } from "../mockData";
 import { AddButton } from "../components/AddButton";
 import Select from "react-select";
+import Modal from "react-modal";
+import { CloseButton } from "../components/CloseButton";
 
 const BalanceAmount = styled.h1<{ mt?: string }>`
   font-size: 2.75rem;
@@ -29,17 +31,63 @@ const options = [
   { value: "day", label: "This day" },
 ];
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "60%",
+    height: "60%",
+    border: 0,
+    borderRadius: "20px",
+    paddingLeft: "44px",
+    paddingRight: "44px",
+  },
+};
+
 export const MyBalance: React.FC = () => {
   const selectRef = useRef<Select | null>(null);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   console.log(selectRef.current);
 
   return (
     <>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <FlexBox ai="center" jc="space-between" mt="16px">
+          <Title>Add stocks</Title>
+          <CloseButton onClick={() => setModalIsOpen(false)} />
+        </FlexBox>
+
+        <Select
+          ref={selectRef}
+          options={options}
+          defaultValue={options[0]}
+          onChange={() => console.log(selectRef.current?.state.value)}
+          styles={{
+            option: (provided) => ({
+              ...provided,
+              backgroundColor: "#ffffff",
+              color: DARK,
+              padding: 20,
+            }),
+          }}
+        />
+      </Modal>
+
       <section>
         <FlexBox mt="64px" ai="center" jc="space-between">
           <Title>My Balance</Title>
-          <AddButton />
+          <AddButton onClick={() => setModalIsOpen(true)} />
         </FlexBox>
 
         <BalanceAmount mt="12px">$465.6</BalanceAmount>
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+// Modal.defaultStyles.overlay.backgroundColor = 'cornsilk';
+Modal.defaultStyles!.overlay!.backgroundColor = "#0000005f";
 
 ReactDOM.render(
   <React.StrictMode>
