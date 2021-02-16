213e762b562d2f865aa8a3f1620380c6e7a4dbf5
bacebu4 <vasua14735@icloud.com>
2021-02-16 14:08:17 +0300

    layout: new stocks modal done

--- src/containers/NewStockModal.tsx
+++ src/containers/NewStockModal.tsx
@@ -2,6 +2,8 @@ import { FlexBox } from "../components/FlexBox";
 import { BaseInput } from "../components/BaseInput";
 import { BaseButton } from "../components/BaseButton";
 import { BaseModal } from "../components/BaseModal";
+import { GrayUppercaseText } from "../components/GrayUppercaseText";
+import { SearchResultCard } from "../components/SearchResultCard";
 
 type NewStockModalProps = {
   setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -25,6 +27,24 @@ export const NewStockModal: React.FC<NewStockModalProps> = ({
           <BaseButton ml="16px">Search</BaseButton>
         </FlexBox>
       </section>
+
+      <section>
+        <GrayUppercaseText mt="32px" mb="16px">
+          Search results
+        </GrayUppercaseText>
+
+        <SearchResultCard name="CHMF" isSelected={false} />
+        <SearchResultCard name="FXIM" isSelected={false} />
+        <SearchResultCard name="FXTB" isSelected />
+        <SearchResultCard name="FXWO" isSelected={false} />
+      </section>
+
+      <FlexBox mt="32px">
+        <BaseButton color="vivid red">Cancel</BaseButton>
+        <BaseButton isSolid ml="16px" color="purple">
+          Apply
+        </BaseButton>
+      </FlexBox>
     </BaseModal>
   );
 };
